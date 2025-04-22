182aaf4640b89b9951fc3cb4fd45aa501a356b80
thorbjørn <tbonvik@gmail.com>
2025-04-22 18:53:28 +0200

    Initial Commit

---
+++ munchies/munchiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  munchiesApp.swift
+//  munchies
+//
+//  Created by Thorbjørn on 22/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct munchiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
